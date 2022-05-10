4d5c381622923c75fbb2f383b13b33ece1e20112
Christoph Stach <christoph.stach@gmail.com>
2022-05-10 15:00:38 +0200

    Add hero patterns background

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,21 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="min-h-screen pb-10 bg-hero-floating-cogs">
+	<nav class="px-10 py-3 mb-10 bg-white shadow-xl">
+		<ul class="flex justify-end gap-3">
+			<li>
+				<a class="block px-3 py-1 border rounded-md" href="/">Home</a>
+			</li>
+			<li>
+				<a class="block px-3 py-1 border rounded-md" href="/blog">Blog</a>
+			</li>
+		</ul>
+	</nav>
+
+	<div class="px-10">
+		<main class="bg-white mx-auto px-16 py-16 rounded-xl shadow-xl max-w-[960px]">
+			<slot />
+		</main>
+	</div>
+</div>
