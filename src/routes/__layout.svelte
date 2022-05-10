69f0f8f9b5073774fa38742c0cce6274c3c3c1b9
Christoph Stach <christoph.stach@gmail.com>
2022-05-10 12:17:27 +0200

    Add layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="page">
+	<nav>
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/blog">Blog</a>
+			</li>
+		</ul>
+	</nav>
+	<main>
+		<slot />
+	</main>
+</div>
