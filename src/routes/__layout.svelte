b3023a9813d95f6e6a1454d86e1feaeae91b8680
Christoph Stach <christoph.stach@gmail.com>
2022-05-10 13:50:30 +0200

    Test resume

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,18 +2,4 @@
 	import '../app.css';
 </script>
 
-<div class="page">
-	<nav>
-		<ul>
-			<li>
-				<a href="/">Home</a>
-			</li>
-			<li>
-				<a href="/blog">Blog</a>
-			</li>
-		</ul>
-	</nav>
-	<main>
-		<slot />
-	</main>
-</div>
+<slot />
