9c1c5d4ed4fe9ca82cf61345d1ec120bbde3463e
Christoph Stach <christoph.stach@gmail.com>
2022-05-21 12:14:25 +0200

    WIP

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,10 @@
+<nav class="px-10 py-3 mb-10 bg-white shadow-xl">
+	<ul class="flex justify-end gap-3">
+		<li>
+			<a class="block px-3 py-1 border rounded-md" href="/">Home</a>
+		</li>
+		<li>
+			<a class="block px-3 py-1 border rounded-md" href="/blog">Blog</a>
+		</li>
+	</ul>
+</nav>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,10 @@
 <script>
+	import Navigation from 'src/components/Navigation.svelte';
 	import '../app.css';
 </script>
 
 <div class="min-h-screen pb-10 bg-hero-floating-cogs">
-	<nav class="px-10 py-3 mb-10 bg-white shadow-xl">
-		<ul class="flex justify-end gap-3">
-			<li>
-				<a class="block px-3 py-1 border rounded-md" href="/">Home</a>
-			</li>
-			<li>
-				<a class="block px-3 py-1 border rounded-md" href="/blog">Blog</a>
-			</li>
-		</ul>
-	</nav>
+	<Navigation />
 
 	<div class="px-10">
 		<main class="bg-white mx-auto px-16 py-16 rounded-xl shadow-xl max-w-[960px]">
