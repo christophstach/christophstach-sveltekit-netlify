91373abeeb6c6fcc7a2c4b3f02555c230154ecf6
Christoph Stach <christoph.stach@gmail.com>
2022-05-21 17:22:27 +0200

    More content

--- src/components/Education.svelte
+++ src/components/Education.svelte
@@ -6,13 +6,27 @@
 			title: 'M. Sc. Angewandte Informatik ',
 			institution: 'HTW Berlin - Hochschule für Technik und Wirtschaft Berlin',
 			description:
-				'At HTW Berlin I am studying applied computer science (Angewandte Informatik). I am currently in the end of my masters. During the time on this university I did serveal projects in the area of bio informatics, deep learning and data science.',
+				'At <a target="_blank" href="https://www.htw-berlin.de/">HTW Berlin</a> I am studying applied computer science (Angewandte Informatik). I am currently in the end of my masters. During the time on this university I did serveal projects in the area of bio informatics, deep learning and data science.',
 			bullets: [
 				'Generative adverserial networks to generate pictures of human faces',
-				'On demand next generation sequencing pipeline for the Covid-SpiNGS project'
+				'Member of the <a target="_blank" href="https://campus-stories.htw-berlin.de/en/jahr/2022/in-drei-stunden-das-virus-identifizieren/">Covid-SpiNGS</a> project, developing a on-demand next generation sequencing pipeline for COVID-19 detection',
+				'Exchange year in Taipei, Taiwan at the <a target="_blank" href="https://www.ntust.edu.tw/">National Taiwan University of science and technologies (NTUST)</a>'
 			],
-			from: dayjs('2020-10-01'),
+			from: dayjs('2019-10-01'),
 			to: dayjs('2023-09-30')
+		},
+
+		{
+			title: 'B. Sc. Angewandte Informatik ',
+			institution: 'HTW Berlin - Hochschule für Technik und Wirtschaft Berlin',
+			description:
+				'I graduated from <a target="_blank" href="https://www.htw-berlin.de/">HTW Berlin</a> with a Bachelor in applied computer science. During this time I specialized in data science with deep learning.',
+			bullets: [
+				'Exchange semester in Cuernavaca, Mexico at <a target="_blank" href="https://uninter.edu.mx/">Universidad Internacional (UNINTER)</a>',
+				'Bachelor thesis about style transfer of paintings to photos with methods of deep learning'
+			],
+			from: dayjs('2016-04-01'),
+			to: dayjs('2019-09-30')
 		}
 	];
 </script>
@@ -20,7 +34,7 @@
 <h2 class="mb-4 border-b-2 border-black text-2xl">Experience</h2>
 
 {#each educationItems as item}
-	<div>
+	<div class="mb-10">
 		<div class="flex">
 			<div class="flex-1">
 				<h3 class="text-lg">{item.title}</h3>
@@ -37,12 +51,12 @@
 			</div>
 		</div>
 
-		<p class="my-2 text-sm">{item.description}</p>
+		<p class="prose prose-sm prose-indigo mb-5 max-w-none">{@html item.description}</p>
 
 		{#if item.bullets.length > 0}
 			<ul class="list-disc text-sm">
 				{#each item.bullets as bullet}
-					<li class="ml-5">{bullet}</li>
+					<li class="prose prose-sm prose-indigo ml-5 max-w-none">{@html bullet}</li>
 				{/each}
 			</ul>
 		{/if}
